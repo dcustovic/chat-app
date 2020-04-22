177ee993bc05a9f8525fd5da1f0d520f51b4d5b9
Dominik Čustović <domcus44@gmail.com>
2020-04-23 00:05:48 +0200

    outputting messages to the DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,15 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
+// message from server
 socket.on('message', message => {
-    console.log(message)
+    console.log(message);
+    outputMessage(message);
+
+    // scroll messages to the bottom
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // message submit
@@ -16,3 +22,14 @@ chatForm.addEventListener('submit', e => {
     // emit message to server
     socket.emit('chatMessage', msg);
 });
+
+// output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
