fbb9512e91203ba4f2ad1d44bfaf24b1243f3976
Dominik Čustović <domcus44@gmail.com>
2020-04-23 00:16:32 +0200

    adding message format

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app);
@@ -10,23 +11,26 @@ const io = socketio(server);
 // setting static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+const botName = 'Chattor Bot';
+
 // run when client connects
 io.on('connection', socket => {
 
     // welcome current user
-    socket.emit('message', 'Welcome to my chatapp!')
+    socket.emit('message', formatMessage(botName, 'Welcome to my chatapp!'));
 
     // broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined a chat.');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined a chat.'));
 
     // run when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat.')
+        io.emit('message', formatMessage(botName, 'A user has left the chat.'));
     });
 
     // listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     });
 
 });
